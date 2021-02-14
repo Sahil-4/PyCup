e2f5ee018280a6d2da9a8ed5936b03518bf924ae
Sahil-4 <www.sahil.875020@gmail.com>
2021-02-14 13:40:16 +0530

    basics and operators

---
+++ pyOperatos.py
@@ -0,0 +1,45 @@
+
+"""
+1. Arithmetic operators
+    +    Addition    x + y
+    -    Subtraction    x - y
+    *    Multiplication    x * y
+    /    Division    x / y
+    %    Modulus    x % y
+    **    Exponentiation    x ** y
+    //    Floor division    x // y
+
+"""
+#Example
+x = 5
+y = 2
+
+#z = x + y
+#z = x - y
+#z = x * y
+#z = x / y
+#z = x ** y
+#z = x % y
+#z = x // y
+
+#print(z)
+
+"""
+2. Assignment operators
+    =    x = 5    x = 5
+    +=    x += 3    x = x + 3
+    -=    x -= 3    x = x - 3
+    *=    x *= 3    x = x * 3
+    /=    x /= 3    x = x / 3
+    %=    x %= 3    x = x % 3
+    //=    x //= 3    x = x // 3
+    **=    x **= 3    x = x ** 3
+    &=    x &= 3    x = x & 3
+    |=    x |= 3    x = x | 3
+    ^=    x ^= 3    x = x ^ 3
+    >>=    x >>= 3    x = x >> 3
+    <<=    x <<= 3    x = x << 3
+
+"""
+#Example
+
